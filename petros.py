219296885847135cb7830f945b23a4f4e2cc657b
Nikos Ioannou <ioannou.nikos@gmail.com>
2014-01-12 01:41:57 +0200

    Major Structural Change
    The structure of the excel file is completely changed and now is much
    more efficient to work with pandas since its on a list base logic that
    can be transformed

--- petros.py
+++ petros.py
@@ -14,31 +14,14 @@ import numpy as np
 #                                 index_col=0,parse_cols="B:C,D:O",na_values=['NA'])
 
 #specify the location of the file
-loc = "petros_new.xls"
+loc = "data.xls"
 #Specify the sheet names
-names = ['maios2012','iounios2012','euro2009','per2010','dim2010']
+names = ['data']
 
-names_voul = ['enotita','dimos','egg','psifisan','eggira','akira','lefka',
-                'nd','siriza','pasok','xa','kke','anel','dimar']
-#create a dictionary of dataframes from the excel file
-frames = {
-          names[0]:pd.read_excel(loc,names[0],header=None,skiprows=1,
-                                 index_col=None,parse_cols="A:N",na_values=['NA']),
-          names[1]:pd.read_excel(loc,names[1],header=0,skiprows=0,
-                                 index_col=0,parse_cols="A:N",na_values=['NA']),
-          names[2]:pd.read_excel(loc,names[2],header=0,skiprows=0,
-                                 index_col=0,parse_cols="A:N",na_values=['NA']),
-          names[3]:pd.read_excel(loc,names[3],header=0,skiprows=0,
-                                 index_col=0,parse_cols="A:AB",na_values=['NA']),
-          names[4]:pd.read_excel(loc,names[4],header=0,skiprows=0,
-                                 index_col=0,parse_cols="A:L",na_values=['NA'])}
+col_names = ['ENOTHTA','DIMOS','EKLOGI','FASI','METRHSH','ARITHMOS']
 
-#Fix column names
-frames[names[0]].columns = names_voul
-print frames[names[0]]
+#Read the data into the data frame
+df = pd.read_excel(loc,'data',header=0,skiprows=0,index_col=None,parse_cols='A:F',na_values=['NA'])
 
-#Work with maios2012
-m12 = frames[names[0]]
-#Group by enotita
-g_m12 = m12.groupby('enotita')
-print g_m12.groups
+
+print df
